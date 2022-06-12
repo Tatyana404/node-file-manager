e6f49cece1413028f748ef2f0e13a1ce14dc64bb
Tatyana404 <kot.kate2020@gmail.com>
2022-06-12 23:21:00 +0300

    refactor: tweaked --cpus output

--- modules/os.js
+++ modules/os.js
@@ -1,9 +1,8 @@
 import { EOL, cpus, homedir, userInfo, arch } from 'os'
-import { cwd } from 'process'
 
 export const operatingSystemInfo = input => {
   const [command] = input.split(' ').slice(-1)
-  
+
   switch (command) {
     case '--EOL':
       console.log(JSON.stringify(EOL))
@@ -15,7 +14,7 @@ export const operatingSystemInfo = input => {
       for (const { model, speed } of cpus()) {
         cpusResult.push({
           model,
-          speed: Math.trunc(speed / 1000)
+          speed: `${(speed / 1000).toFixed(1)} GHz`
         })
       }
 
@@ -35,6 +34,6 @@ export const operatingSystemInfo = input => {
 
       break
     default:
-      return console.error(`Operation failed\nYou are currently in ${cwd()}`)
+      return console.error('Invalid input')
   }
 }
